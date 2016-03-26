a18189d99a3c7f3f67433261b7dc9ce31f79d09c
smicl11 <bryan.smith094@gmail.com>
2016-03-26 16:31:56 -0700

    removed unnecessary code in app.js to streamline code

--- scripts/app.js
+++ scripts/app.js
@@ -6,30 +6,17 @@ console.log("JS has loaded!");
 $(document).ready(function() {
   console.log("the document is ready");
 
-  var playerOne = new Player (81, 0);
-  var playerTwo = new Player (80, 0);
+  //create function with a for loop counting number of 'keyup' to 34 then alert winner based on who reached 34 keypresses
 
-  console.log(playerTwo);
-  console.log(playerOne);
-
-  playerOne.move();
-  playerTwo.move();
-
-  function Player(keybutton, win) {
-    this.keybutton = keybutton;
-    this.win = win; //create function with a for loop counting number of 'keyup' to 34 then alert winner based on who reached 34 keypresses first. 
-    this.move = function() {
-      $(window).on('keydown', function moveRight(event) {
-          if(event.keyCode === 81) {
-            $('#player1').animate({ 'left': '+=10px' }, 0);
-          } else if(event.keyCode === 80) {
-            $('#player2').animate({ 'left': '+=10px' }, 0);
-          }
-        });
-      };
+  function vroomVroom() {
+    $(window).on('keydown', function moveRight(event) {
+      if(event.keyCode === 81) {
+        $('#player1').animate({ 'left': '+=10px' }, 0);
+      } else if(event.keyCode === 80) {
+        $('#player2').animate({ 'left': '+=10px' }, 0);
+      }
+    });
   }
-
-
 
 });
 
